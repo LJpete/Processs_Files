6ab5516f89e39b2424da20ff81503ec13d0ee97a
LJPete <petelac86@gmail.com>
2026-02-09 22:03:18 +1030

    cleanup

--- App.xaml.cs
+++ App.xaml.cs
@@ -3,32 +3,19 @@ using Microsoft.UI.Xaml;
 
 namespace App1
 {
-    /// <summary>
-    /// Provides application-specific behavior to supplement the default Application class.
-    /// </summary>
     public partial class App : Application
     {
-        /// <summary>
-        /// Checks for the existence of the Arguments.template file in the executable directory.
-        /// Throws FileNotFoundException if not found.
-        /// </summary>
-
         private Window? _window;
-
-        /// <summary>
-        /// Initializes the singleton application object.  This is the first line of authored code
-        /// executed, and as such is the logical equivalent of main() or WinMain().
-        /// </summary>
+            
         public App()
         {
+   
             InitializeComponent();
         }
 
-        /// <summary>
-        /// Invoked when the application is launched.
-        /// </summary>
-        /// <param name="args">Details about the launch request and process.</param>
-        /// 
+  
+        /// <param name="args"
+        
         protected override void OnLaunched(Microsoft.UI.Xaml.LaunchActivatedEventArgs args)
         {
             _window = new MainWindow();
